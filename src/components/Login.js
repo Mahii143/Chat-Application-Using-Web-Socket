b16b26cdb9ff0a403cb31de9288230af36e25af8
Mahii143 <2012091@nec.edu.in>
2023-06-18 19:06:24 +0530

    login fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,9 +4,10 @@ import { Link, useNavigate } from "react-router-dom";
 import url from "../endpoint.json";
 
 const Login = ({ setToken }) => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const Navigate = useNavigate();
-  const getToken = async (username) => {
+  const getToken = async (_email, _password) => {
     const endpoint = url.endpoint + "login";
     try {
       await fetch(endpoint, {
@@ -15,7 +16,7 @@ const Login = ({ setToken }) => {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username: username }),
+        body: JSON.stringify({ email: _email, password: _password }),
       })
         .then((res) => {
           if (!res.ok) {
@@ -34,26 +35,43 @@ const Login = ({ setToken }) => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await getToken(username);
-    // console.log("form submitted", username);
+    await getToken(email, password);
   };
   return (
     <form onSubmit={handleSubmit} className="login-form">
       <h1>Good to see you again</h1>
       <p>Welcome to Mahir chatapp!</p>
       <label htmlFor="uname" className="login-input-label">
-        USER NAME
+        Email
         <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          type="email"
+          name="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           id="uname"
-          placeholder="user name"
+          placeholder="email"
+        />
+      </label>
+      <label htmlFor="pwd" className="login-input-label">
+        Password
+        <input
+          type="password"
+          name="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="pwd"
+          placeholder="password"
         />
       </label>
       <button>sign in</button>
-      <p>Don't have an account? <Link to='../signup' className='route-login'>sign up</Link> </p>
+      <p>
+        Don't have an account?{" "}
+        <Link to="../signup" className="route-login">
+          sign up
+        </Link>{" "}
+      </p>
     </form>
   );
 };
